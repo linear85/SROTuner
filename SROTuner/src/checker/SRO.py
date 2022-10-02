df8bb6b3d351bbc61691aa46a33d5cb08e0b434c
VScoldness <yyao26@crimson.ua.edu>
2022-10-02 09:52:38 -0700

    update test folder

--- SROTuner/src/checker/SRO.py
+++ SROTuner/src/checker/SRO.py
@@ -1,6 +1,8 @@
+from typing import Tuple
 from ovito.io import import_file
 from ovito.data import NearestNeighborFinder
 import numpy as np
+from tqdm import tqdm
 
 class SRO:
     def __init__(self, dataFile: str, structure: str, elements: int, composition = None) -> None:
@@ -14,26 +16,23 @@ class SRO:
         assert (structure == "BCC" or structure == 'FCC'), "only support BCC and FCC structure"
         self.neighCount = 8 if structure == 'BCC' else 12
         
-
     # get the composition (np)
-    def getComposition(self) -> np:
+    def getComposition(self) -> np.ndarray:
         tmp = np.array([0 for _ in range(self.elements)])
         for i in self.eleTypes:
             tmp[i-1] += 1
         return tmp/self.data.particles.count
     
     # calculate the WCP
-    def WCP(self):
+    def WCP(self) -> np.ndarray:
         finder = NearestNeighborFinder(self.neighCount, self.data)
-        for idx in range(self.data.particles.count):
+        for idx in tqdm(range(self.data.particles.count)):
             eleType, counts = self.localAtomicEnv(idx, finder)
             self.single_atom_WCP(eleType, self.neighCount, counts)
         SRO_para = self.reshapeWCP()
         return SRO_para
         
-    
-
-    def reshapeWCP(self) -> np:
+    def reshapeWCP(self) -> np.ndarray:
         SRO_para = []
         for sro in self.SRO_WCP:
             tmp = sum(sro)/len(sro)
@@ -42,7 +41,8 @@ class SRO:
         SRO_para = SRO_para.reshape(self.elements, self.elements)
         return SRO_para
 
-    def localAtomicEnv(self, idx: int, finder: object):
+    # get the type of central atom and count the number of each element in first shell
+    def localAtomicEnv(self, idx: int, finder: NearestNeighborFinder) -> Tuple[int, list[int]]:
         eleType = self.eleTypes[idx]
         counts  = [0 for _ in range(self.elements)]
         for neigh in finder.find(idx):
@@ -58,34 +58,5 @@ class SRO:
             tmp_SRO  = 1 - num/(comp*N)
             self.SRO_WCP[base+i].append(tmp_SRO)
             i += 1
-        return 
-
-
-
-class BCC(SRO):
-    def __init__(self, dataFile: str, composition=None) -> None:
-        super().__init__(dataFile, composition)
-        self.neighCount = 8
-    
-    def WCP(self) -> np:
-        finder = NearestNeighborFinder(self.neighCount, self.data)
-        for idx in range(self.data.particles.count):
-            eleType, counts = self.localAtomicEnv(idx, finder)
-            self.single_atom_WCP(eleType, self.neighCount, counts)
-        SRO_para = self.reshapeWCP()
-        return SRO_para
-
-
-class FCC(SRO):
-    def __init__(self, dataFile: str, composition=None) -> None:
-        super().__init__(dataFile, composition)
-        self.neighCount = 12
-
-    def WCP(self) -> np:
-        finder = NearestNeighborFinder(self.neighCount, self.data)
-        for idx in range(self.data.particles.count):
-            eleType, counts = self.localAtomicEnv(idx, finder)
-            self.single_atom_WCP(eleType, self.neighCount, counts)
-        SRO_para = self.reshapeWCP()
-        return SRO_para
+        return
 
---
+++ tests/SRO_Tuner.py
@@ -0,0 +1,68 @@
+import numpy as np
+import os
+
+from RHEA import RHEA
+from SROTuner import SRO
+from SROTuner import Quaternary
+from SROTuner import Ternary
+np.set_printoptions(suppress=True)
+
+
+def testSRO_Tuner(testFile: str, WCP: np, element: int, structure: str, tolerance=20):
+    baseDir = os.getcwd()
+    outputStr = os.path.join(baseDir, "tmp.lmp")
+    match element:
+        case 3: tuner = Ternary(testFile, WCP, outputStr, structure, tolerance) 
+        case 4: tuner = Quaternary(testFile, WCP, outputStr, structure, tolerance)
+    tuner.adjustSRO()
+    WCP2 = SRO(outputStr, structure, element).WCP()
+    print(WCP2)
+    # print(WCP)
+    os.remove(outputStr)
+    return
+
+
+
+def testTuner_BCC_Ternary_Equiatomic():
+    baseDir = os.getcwd()
+    inputFile = os.path.join(baseDir, "SRO_Tuner", "BCC", "ternary", "MoNbTa_Random.lmp")
+    WCP  = np.array([[ 0.64082326,  0.06680182, -0.70809454],
+                     [ 0.06680182, -0.40116626,  0.3343155 ],
+                     [-0.70809454,  0.3343155,   0.37429779]])
+    testSRO_Tuner(inputFile, WCP, 3, "BCC")
+
+
+def testTuner_BCC_Quaternary_Equiatomic():
+    baseDir = os.getcwd()
+    inputFile = os.path.join(baseDir, "SRO_Tuner", "BCC", "quaternary", "Mo25Nb25Ta25W25_Random.lmp")
+    WCP = RHEA().getWCP_300K()["25_25_25_25"]
+    testSRO_Tuner(inputFile, WCP, 4, "BCC")
+
+
+def testTuner_FCC_Ternary_Equiatomic():
+    baseDir = os.getcwd()
+    inputFile = os.path.join(baseDir, "SRO_Tuner", "FCC", "ternary", "FeCoNi_random.lmp")
+    WCP  = np.array([[ 0.3,  0.1, -0.4],
+                     [ 0.1, -0.2,  0.1],
+                     [-0.4,  0.1,  0.3]])
+    testSRO_Tuner(inputFile, WCP, 3, "FCC")
+
+
+def testTuner_FCC_Quaternary_Equiatomic():
+    baseDir = os.getcwd()
+    inputFile = os.path.join(baseDir, "SRO_Tuner", "FCC", "quaternary", "FeCoCrNi_Random.lmp")
+    WCP  = np.array([[0.1, 	-0.1,  -0.1,	0.1],
+                     [-0.1,	-0.2, 	0.2,    0.1],
+                     [-0.1,	 0.2,	0.1,   -0.2],
+                     [0.1,   0.1,  -0.2,	0  ]])
+    testSRO_Tuner(inputFile, WCP, 4, "FCC")
+
+
+
+
+# testTuner_BCC_Ternary_Equiatomic()
+# testTuner_BCC_Quaternary_Equiatomic()
+# testTuner_FCC_Ternary_Equiatomic()
+# testTuner_FCC_Quaternary_Equiatomic()
+
+
